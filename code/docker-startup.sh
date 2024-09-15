073863b30f55226d64f215fc0005e2796b3a3d72
s0evil <examverbenas@gmail.com>
2024-09-15 10:00:39 +0530

    Delete code directory

--- code/docker-startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Start the parsing service
-cd /app/utils/parsing/unstructured-api
-make run-web-app &
-
-# Navigate to the specific kit directory and execute commands
-cd /app
-exec "$@"
